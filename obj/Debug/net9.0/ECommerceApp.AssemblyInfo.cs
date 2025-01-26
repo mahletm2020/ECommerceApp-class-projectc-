b5626830f9e2db98e64a69fcab79349dbe6b9e4f
Mahlet Mulugeta <161637299+mahletm2020@users.noreply.github.com>
2025-01-26 13:30:11 -0800

    auth  some change

--- obj/Debug/net9.0/ECommerceApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/ECommerceApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da66ff34f5aa674606c80262181ff99bf957d71a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a9b3857aac0d01cfd2e16c092596a9fb5a35559")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
